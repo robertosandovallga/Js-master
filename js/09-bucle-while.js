71ec03238ca089b2e32684592e96fe9b8d003110
robertosandovalliga <roberto.sandoval.lga@gmail.com>
2024-09-04 14:30:36 -0700

    09-bucle-while

---
+++ js/09-bucle-while.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Bucle While // mientras se cumpla una condicion s
+
+var year = 2018;
+
+while(year <= 2051){
+// Ejecuta esto
+console.log("Estamos en el año:"+year);
+
+// Operador de incremento
+year++;
+}
+
+while(year != 1991){
+    // Ejecuta esto
+    console.log("Estamos en el año:"+year);
+    
+    // Operador de decremento
+    year--;
+}
