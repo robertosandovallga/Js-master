03e778b8f089d4ad1d0447a7ef79b652cabbffe0
robertosandovalliga <roberto.sandoval.lga@gmail.com>
2024-09-04 15:05:50 -0700

    10-ventanas

---
+++ js/10-ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Alertas
+//alert("esta es mi alerta!!");
+//alert("Esta es mi alerta de texto");
+
+//Confirmacion
+//var miResultado = confirm("¿Estas seguro que quieres continuar?");
+
+//console.log(miResultado);
+
+// Ingreso de datos
+var miResultado = prompt("¿Que edad tienes?", 18);
+
+//typeof te regresa el tipo de datos del valor
+console.log(typeof miResultado);
+
