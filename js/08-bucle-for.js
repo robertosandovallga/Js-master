d04921b3694b910955982745d02962a83508da22
robertosandovalliga <roberto.sandoval.lga@gmail.com>
2024-09-04 14:15:02 -0700

    08-bucle-for

---
+++ js/08-bucle-for.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Bucle For
+// Bucle es una estructura de control que se repite varias veces
+
+var numero = 100;
+// i es el contador
+// ++ le suma el valor a la variable
+for(var i = 0; i <= numero; i++){
+
+    console.log(" Vamos por el numero: "+i); // concatenamos el texto con la variable
+    debugger; //Esto nos sirve para debuguear.
+    // Nos llevara a la pestaña Sources
+    // En la consola, en la seccion switch podemos colocar las variables para ir viendo su valor.
+    // Para colocar un breackpoint en el codigo solo damos click en la linea y se pondra azul.
+}
+
+
+
